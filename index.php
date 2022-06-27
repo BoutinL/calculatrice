4a56540286e8fdc3e6c508667d40333c30ba37a7
unknown <boutin.loic@hotmail.com>
2022-06-27 15:46:36 +0200

    Fin des fonctions

--- index.php
+++ index.php
@@ -10,24 +10,58 @@
     <?php
     echo "
     <h1>Calculatrice en php</h1>
-    <form action='storage.php' method='POST'>
-        <span>Nombre 1:</span>
-        <input type='text' name='nombre1'>
-        <span>Nombre 2:</span>
-        <input type='text' name='nombre2'>
+    <form action='index.php' method='POST'>
+        <label>
+            <span>Nombre 1:</span>
+            <input type='text' name='nombre1'>
+        </label>
+        <label>
+            <span>Nombre 2:</span>
+            <input type='text' name='nombre2'>
+        </label>
         <fieldset>
             <legend>Sélectioner un opérateur</legend>
-            <input type='radio' id='addition' name='addition'>
-            <label for'addition'>Addition</label>
-            <input type='radio' id='soustraction' name='soustraction'>
-            <label for'soustraction'>Soustraction</label>
-            <input type='radio' id='multiplication' name='multiplication'>
-            <label for'multiplication'>Multiplication</label>
-            <input type='radio' id='division' name='division'>
-            <label for'division'>Division</label>
+            <label>
+                Addition
+                <input type='radio' id='addition' name='operateur' value='addition' checked>
+            </label>
+            <label>
+                Soustraction
+                <input type='radio' id='soustraction' name='operateur' value='soustraction'>
+            </label>
+            <label>
+                Multiplication
+                <input type='radio' id='multiplication' name='operateur' value='multiplication'>
+            </label>
+            <label>
+                Division
+                <input type='radio' id='division' name='operateur' value='division'>
+            </label>
         </fieldset>
         <input type='submit'>
     </form>";
+    $nombre1 = intval($_POST['nombre1']);
+    $nombre2 = intval($_POST['nombre2']);
+    $result = intval('');
+    echo "Nombre 1: $nombre1 <br> Nombre 2: $nombre2 <br> Résultat: ";
+    switch($_POST['operateur']){
+        case 'addition':
+            $result = $nombre1 + $nombre2;
+            echo "$result";
+            break;
+        case 'soustraction':
+            $result = $nombre1 - $nombre2;
+            echo "$result";
+            break;
+        case 'multiplication':
+            $result = $nombre1 * $nombre2;
+            echo "$result";
+            break;
+        case 'division':
+            $result = $nombre1 / $nombre2;
+            echo "$result";
+            break;
+    };
     ?>
 </body>
 </html>

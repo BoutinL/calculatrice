51a669a1d5822ae3c1ea69be0929c1942378eec7
unknown <boutin.loic@hotmail.com>
2022-06-28 13:24:43 +0200

    Fin des messages d'erreurs

--- index.php
+++ index.php
@@ -8,60 +8,65 @@
 </head>
 <body>
     <?php
-    echo "
-    <h1>Calculatrice en php</h1>
-    <form action='index.php' method='POST'>
-        <label>
-            <span>Nombre 1:</span>
-            <input type='text' name='nombre1'>
-        </label>
-        <label>
-            <span>Nombre 2:</span>
-            <input type='text' name='nombre2'>
-        </label>
-        <fieldset>
-            <legend>Sélectioner un opérateur</legend>
-            <label>
-                Addition
-                <input type='radio' id='addition' name='operateur' value='addition' checked>
-            </label>
-            <label>
-                Soustraction
-                <input type='radio' id='soustraction' name='operateur' value='soustraction'>
-            </label>
-            <label>
-                Multiplication
-                <input type='radio' id='multiplication' name='operateur' value='multiplication'>
-            </label>
-            <label>
-                Division
-                <input type='radio' id='division' name='operateur' value='division'>
-            </label>
-        </fieldset>
-        <input type='submit'>
-    </form>";
-    $nombre1 = intval($_POST['nombre1']);
-    $nombre2 = intval($_POST['nombre2']);
-    $result = intval('');
-    echo "Nombre 1: $nombre1 <br> Nombre 2: $nombre2 <br> Résultat: ";
-    switch($_POST['operateur']){
-        case 'addition':
-            $result = $nombre1 + $nombre2;
-            echo "$result";
-            break;
-        case 'soustraction':
-            $result = $nombre1 - $nombre2;
-            echo "$result";
-            break;
-        case 'multiplication':
-            $result = $nombre1 * $nombre2;
-            echo "$result";
-            break;
-        case 'division':
-            $result = $nombre1 / $nombre2;
-            echo "$result";
-            break;
-    };
+        echo "
+            <h1>Calculatrice en php</h1>
+            <form action='index.php' method='POST'>
+                <label>
+                    <span>Nombre 1:</span>
+                    <input type='text' name='nombre1'>
+                </label>
+                <br>
+                <label>
+                    <span>Nombre 2:</span>
+                    <input type='text' name='nombre2'>
+                </label>
+                <fieldset>
+                    <legend>Sélectioner un opérateur</legend>
+                    <label>
+                        Addition
+                        <input type='radio' id='addition' name='operateur' value='addition' checked><br>
+                    </label>
+                    <label>
+                        Soustraction
+                        <input type='radio' id='soustraction' name='operateur' value='soustraction'><br>
+                    </label>
+                    <label>
+                        Multiplication
+                        <input type='radio' id='multiplication' name='operateur' value='multiplication'><br>
+                    </label>
+                    <label>
+                        Division
+                        <input type='radio' id='division' name='operateur' value='division'>
+                    </label>
+                </fieldset>
+                <input type='submit' name='submit' value='Calculer'>
+            </form>
+        ";
+        if(isset($_POST['submit'])){
+            $nombre1 =  str_replace(',' , '.' , $_POST['nombre1']);
+            $nombre2 = str_replace(',' , '.' , $_POST['nombre2']);
+            echo "Nombre 1: $nombre1 <br> Nombre 2: $nombre2 <br> Résultat: ";
+        }
+        if (is_numeric($nombre1) === true && is_numeric($nombre2) === true){
+            $nombre1 = floatval($nombre1);
+            $nombre2 = floatval($nombre2);
+            switch($_POST['operateur']){
+                case 'addition':
+                    echo $nombre1 + $nombre2;
+                    break;
+                case 'soustraction':
+                    echo $nombre1 - $nombre2;
+                    break;
+                case 'multiplication':
+                    echo $nombre1 * $nombre2;
+                    break;
+                case 'division':
+                    echo $nombre2 == 0 ? "Vous ne pouvez pas diviser un nombre par 0 !" : $nombre1 / $nombre2 ;
+                    break;
+            };
+        }elseif(is_numeric($nombre1) === false || is_numeric($nombre2) === false){
+            echo "Veuillez renseigner une chaîne numérique !";
+        }
     ?>
 </body>
 </html>
